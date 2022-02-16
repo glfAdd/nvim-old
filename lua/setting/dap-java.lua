d5f4e8487590619ee3ddd33c04d47288326e8348
gonglongfei <2239660080@qq.com>
2022-02-16 18:50:26 +0800

    java dap

--- lua/setting/dap-java.lua
+++ lua/setting/dap-java.lua
@@ -1,16 +1,16 @@
+local vim = vim
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = '~/Desktop/learn/test/' .. project_name
+
 local bundles = {
-  vim.fn.glob("/opt/neovim-dap/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"),
-};
+  vim.fn.glob("/opt/neovim-dap/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar")
+}
 vim.list_extend(bundles, vim.split(vim.fn.glob("/opt/neovim-dap/vscode-java-test/server/*.jar"), "\n"))
 
--- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
-local config = {
-  -- The command that starts the language server
-  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
-  cmd = {
 
-    -- 💀
-    'java', -- or '/path/to/java11_or_newer/bin/java'
+local config = {
+  cmd = {
+    'java',
     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
     '-Dosgi.bundles.defaultStartLevel=4',
     '-Declipse.product=org.eclipse.jdt.ls.core.product',
@@ -20,58 +20,69 @@ local config = {
     '--add-modules=ALL-SYSTEM',
     '--add-opens', 'java.base/java.util=ALL-UNNAMED',
     '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-    '-jar', '/opt/neovim-dap/jdt-language-server-1.8.0-202201261434/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
-    '-configuration', '/opt/neovim-dap/jdt-language-server-1.8.0-202201261434/config_linux',
-    -- 💀
-    -- See `data directory configuration` section in the README
-    -- '-data', '/path/to/unique/per/project/workspace/folder'
-    '-data', '~/Desktop/learn/test'
+    '-jar', '/opt/neovim-dap/jdt-language-server-1.9.0/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
+    '-configuration', '/opt/neovim-dap/jdt-language-server-1.9.0/config_linux',
+    '-data', workspace_dir
   },
-
-  -- 💀
-  -- This is the default if not provided, you can remove it. Or adjust as needed.
-  -- One dedicated LSP server & client will be started per unique root_dir
-  -- root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
+  root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
 
   -- Here you can configure eclipse.jdt.ls specific settings
   -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-  -- for a list of options
-  -- settings = {
-  --   java = {
-  --   }
-  -- },
+  settings = {
+    java = {
+    }
+  },
 
-  -- Language server `initializationOptions`
-  -- You need to extend the `bundles` with paths to jar files
-  -- if you want to use additional eclipse.jdt.ls plugins.
-  --
-  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
-  --
-  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
   init_options = {
-    bundles = bundles
+    bundles = bundles;
   },
 }
+
+config['on_attach'] = function(client, bufnr)
+  -- With `hotcodereplace = 'auto' the debug adapter will try to apply code changes
+  -- you make during a debug session immediately.
+  -- Remove the option if you do not want that.
+  require('jdtls').setup_dap({ hotcodereplace = 'auto' })
+end
+
+
+
 -- This starts a new client & server,
 -- or attaches to an existing client & server depending on the `root_dir`.
 require('jdtls').start_or_attach(config)
 
+
 local dap = require('dap')
 dap.configurations.java = {
-    -- classPaths = {},
-    -- projectName = "yourProjectName",
-    --
-    javaExec = "/home/glfadd/.sdkman/candidates/java/current/bin/java",
-    --
-    -- -- If using the JDK9+ module system, this needs to be extended
-    -- -- `nvim-jdtls` would automatically populate this property
-    -- modulePaths = {},
-    -- request = "launch",
-            type= "java",
-            name= "Launch Current File",
-            request= "launch",
-            mainClass= "${file}"
+  {
+    type= "java",
+    name= "java-script",
+    request= "launch",
+    mainClass= "${file}"
+  },
+  {
+    type= "java",
+    name= "java-script2",
+    request= "launch",
+    mainClass= "${file}"
+  },
+
+  -- {
+  --   -- classPaths = {},
+  --   -- projectName = "yourProjectName",
+  --   --
+  --   -- javaExec = "/home/glfadd/.sdkman/candidates/java/current/bin/java",
+  --   --
+  --   -- -- If using the JDK9+ module system, this needs to be extended
+  --   -- -- `nvim-jdtls` would automatically populate this property
+  --   -- modulePaths = {},
+  --   -- request = "launch",
+  --   type= "java",
+  --   name= "Launch Current File2",
+  --   request= "launch",
+  --   -- mainClass= "${file}"
+  --   mainClass= "A"
+  -- },
 }
-
-
-
+require'jdtls'.test_class()
+require'jdtls'.test_nearest_method()
--- lua/setting/telescope.lua
+++ lua/setting/telescope.lua
@@ -26,6 +26,8 @@ require('telescope').setup {
 }
 
 require'telescope'.load_extension('fzf')
+require('telescope').load_extension('dap')
+
 
 
 
