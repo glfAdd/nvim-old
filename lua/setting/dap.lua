7692935db0521ed152c5fb16baaf196e35fc9a3c
gonglongfei <2239660080@qq.com>
2022-02-18 02:30:47 +0800

    dap python launch

--- lua/setting/dap.lua
+++ lua/setting/dap.lua
@@ -1,4 +1,7 @@
--- 如果导入空的 json 文件会报错
+-- 1. 如果导入空的 json 文件会报错
+-- 2. java 项目启动失败的时候要删除 workspace 对应的项目文件后重试
 
+-- dap jvava config
 require('dap.ext.vscode').load_launchjs('/home/glfadd/.config/nvim/vscode-launch/java.json');
--- require('dap.ext.vscode').load_launchjs('/home/glfadd/.config/nvim/vscode-launch/python.json');
+-- dap python config
+require('dap.ext.vscode').load_launchjs('/home/glfadd/.config/nvim/vscode-launch/python.json');
