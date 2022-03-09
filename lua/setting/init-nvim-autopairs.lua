e644b88a702d01e83fca83edccb792206f3c9a14
gonglongfei <2239660080@qq.com>
2022-03-09 19:29:16 +0800

    fix

--- lua/setting/init-nvim-autopairs.lua
+++ lua/setting/init-nvim-autopairs.lua
@@ -1,9 +1,9 @@
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
-cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
-
-require('nvim-autopairs').setup({
-  enable_check_bracket_line = false,
-  ignored_next_char = "[%w%.]"
-})
+-- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+-- local cmp = require('cmp')
+-- cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+-- cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
+--
+-- require('nvim-autopairs').setup({
+--   enable_check_bracket_line = false,
+--   ignored_next_char = "[%w%.]"
+-- })
