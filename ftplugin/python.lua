20df49ba75c616357254f9806e85ba2bd208194f
gonglongfei <2239660080@qq.com>
2023-01-08 15:14:36 +0800

    dap update

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,56 +1,56 @@
-
-require'dap-python'.setup('python')
-require'dap-python'.test_runner = 'pytest'
-local dap = require('dap')
-local vim = vim
-
-
--- dap.configurations.python = dap.configurations.python or {}  -- 保留系统自带的
-dap.configurations.python = {} -- 删除系统自带的
-
--- simple python script
-table.insert(dap.configurations.python, {
-  type = 'python',
-  request = 'launch',
-  name = 'python-script',
-  program = '${file}',
-  console = 'integratedTerminal',
-})
-
-
--- flask server
-table.insert(dap.configurations.python, {
-  type = 'python';
-  request = 'launch';
-  name = 'python-flask';
-  console = 'integratedTerminal';
-  module = 'flask';
-  -- args = {'run'};
-  args = {'run', '--port', '10469'};
-  env = {
-    FLASK_APP= "${file}";
-    FLASK_ENV= "development";
-    FLASK_DEBUG= "0"
-  }
-})
-
-
--- flask server args
--- 启动示例
--- run --port 10469
-table.insert(dap.configurations.python, {
-  type = 'python';
-  request = 'launch';
-  name = 'python-flask-args';
-  console = 'integratedTerminal';
-  module = 'flask';
-  args = function()
-    local args_string = vim.fn.input('Arguments: ')
-    return vim.split(args_string, " +")
-  end;
-  env = {
-    FLASK_APP= "${file}";
-    FLASK_ENV= "development";
-    FLASK_DEBUG= "0"
-  }
-})
+--
+-- require'dap-python'.setup('python')
+-- require'dap-python'.test_runner = 'pytest'
+-- local dap = require('dap')
+-- local vim = vim
+--
+--
+-- -- dap.configurations.python = dap.configurations.python or {}  -- 保留系统自带的
+-- dap.configurations.python = {} -- 删除系统自带的
+--
+-- -- simple python script
+-- table.insert(dap.configurations.python, {
+--   type = 'python',
+--   request = 'launch',
+--   name = 'python-script',
+--   program = '${file}',
+--   console = 'integratedTerminal',
+-- })
+--
+--
+-- -- flask server
+-- table.insert(dap.configurations.python, {
+--   type = 'python';
+--   request = 'launch';
+--   name = 'python-flask';
+--   console = 'integratedTerminal';
+--   module = 'flask';
+--   -- args = {'run'};
+--   args = {'run', '--port', '10469'};
+--   env = {
+--     FLASK_APP= "${file}";
+--     FLASK_ENV= "development";
+--     FLASK_DEBUG= "0"
+--   }
+-- })
+--
+--
+-- -- flask server args
+-- -- 启动示例
+-- -- run --port 10469
+-- table.insert(dap.configurations.python, {
+--   type = 'python';
+--   request = 'launch';
+--   name = 'python-flask-args';
+--   console = 'integratedTerminal';
+--   module = 'flask';
+--   args = function()
+--     local args_string = vim.fn.input('Arguments: ')
+--     return vim.split(args_string, " +")
+--   end;
+--   env = {
+--     FLASK_APP= "${file}";
+--     FLASK_ENV= "development";
+--     FLASK_DEBUG= "0"
+--   }
+-- })
--- lua/setting/init-dap.lua
+++ lua/setting/init-dap.lua
@@ -1,1 +1,115 @@
+--[[
+ftplugin 配置文件目录
+    python.lua
+
+路径不能使用相对路径, 必须用绝对路径
+
+-- 当前路径
+local cwd = vim.fn.getcwd()
+--]]
 local dap = require("dap")
+local vim = vim
+
+-- ##########################################################
+-- ui
+-- ##########################################################
+local dap_breakpoint_color = {
+	breakpoint = {
+		ctermbg = 0,
+		fg = "#993939",
+		bg = "#31353f",
+	},
+	logpoing = {
+		ctermbg = 0,
+		fg = "#61afef",
+		bg = "#31353f",
+	},
+	stopped = {
+		ctermbg = 0,
+		fg = "#98c379",
+		bg = "#31353f",
+	},
+}
+
+vim.api.nvim_set_hl(0, "DapBreakpoint", dap_breakpoint_color.breakpoint)
+vim.api.nvim_set_hl(0, "DapLogPoint", dap_breakpoint_color.logpoing)
+vim.api.nvim_set_hl(0, "DapStopped", dap_breakpoint_color.stopped)
+
+local dap_breakpoint = {
+	error = {
+		text = "",
+		texthl = "DapBreakpoint",
+		linehl = "DapBreakpoint",
+		numhl = "DapBreakpoint",
+	},
+	condition = {
+		text = "ﳁ",
+		texthl = "DapBreakpoint",
+		linehl = "DapBreakpoint",
+		numhl = "DapBreakpoint",
+	},
+	rejected = {
+		text = "",
+		texthl = "DapBreakpint",
+		linehl = "DapBreakpoint",
+		numhl = "DapBreakpoint",
+	},
+	logpoint = {
+		text = "",
+		texthl = "DapLogPoint",
+		linehl = "DapLogPoint",
+		numhl = "DapLogPoint",
+	},
+	stopped = {
+		text = "",
+		texthl = "DapStopped",
+		linehl = "DapStopped",
+		numhl = "DapStopped",
+	},
+}
+
+vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
+vim.fn.sign_define("DapBreakpointCondition", dap_breakpoint.condition)
+vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
+vim.fn.sign_define("DapLogPoint", dap_breakpoint.logpoint)
+vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
+
+-- ##########################################################
+-- python
+-- ##########################################################
+-- 调试器使用 debugpy
+dap.adapters.python = {
+	-- type 表示启动调试器的方式
+	-- executable 表示由客户端自行启动调试器
+	type = "executable",
+	-- 启动调试器的命令
+	command = "/home/gong/.config/nvim/source/p3.11.0-venv/bin/python",
+	-- python 调试工具 debugpy 是一个 Python 的第三方模块, 使用 python -m debugpy.adapter 来启动这个调试器
+	args = { "-m", "debugpy.adapter" },
+}
+
+-- 启动配置
+dap.configurations.python = {
+	{
+		-- 使用哪个调试器，跟我们之前配置的 dap.adapters相关
+		type = "python",
+		-- 调试的方式
+		-- request = "attach", -- 附加到一个已有的进程
+		request = "launch", -- 启动新进程调试
+		name = "python base",
+		-- 当前文件
+		program = "${file}",
+		pythonPath = function()
+			return "/usr/bin/python"
+		end,
+	},
+	{
+		type = "python",
+		request = "launch",
+		name = "python base venv",
+		program = "${file}",
+		pythonPath = function()
+			return os.getenv("VIRTUAL_ENV") .. "/bin/python"
+		end,
+	},
+}
